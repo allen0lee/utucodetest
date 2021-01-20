d6d6bb2a39aa66109ae70545efb0e15476f55dc5
Alan <allen0lee@gmail.com>
2021-01-20 14:32:13 +1100

    enable cors

--- main.rb
+++ main.rb
@@ -7,6 +7,14 @@ require_relative 'db_config'
 require_relative 'models/crypto.rb'
 require 'sinatra/cross_origin'
 
+set :bind, '0.0.0.0'
+configure do
+  enable :cross_origin
+end
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
+  
 
 get "/" do
   erb(:index)
@@ -51,4 +59,11 @@ get "/crypto_api" do
     |a, b| b["Mkt Cap"].gsub(/[^0-9]/,'').to_i <=> a["Mkt Cap"].gsub(/[^0-9]/,'').to_i
   }.to_json
 
+end
+
+options "*" do
+  response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  200
 end
