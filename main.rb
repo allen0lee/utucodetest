d119b1e887d6410875548ad492bdd1f33114d610
Alan <allen0lee@gmail.com>
2021-01-21 15:45:32 +1100

    refactor code

--- main.rb
+++ main.rb
@@ -1,9 +1,7 @@
 require 'sinatra'
 require 'pry'
 require 'sinatra/reloader' if settings.development?
-require 'httparty'
 require 'active_record'
-require_relative 'db_config'
 require_relative 'models/crypto.rb'
 require 'sinatra/cross_origin'
 
